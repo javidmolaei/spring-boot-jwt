a7ad2f494db52e280e72db2cbe2c6dbc06f3d24e
javidmolaei <javidmolaei@gmail.com>
2022-09-11 00:45:39 -0600

    Initial commit

---
+++ src/test/java/ir/j2m/springbootjwt/SpringBootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.j2m.springbootjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
